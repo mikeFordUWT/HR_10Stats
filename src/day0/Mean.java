89fbc309f57c572c9259f46d88be01c5834cef50
michaelford <fordm13@uw.edu>
2016-09-13 15:17:26 -0700

    Initial commit

---
+++ src/day0/Mean.java
@@ -0,0 +1,26 @@
+package day0;
+
+import java.util.Scanner;
+
+/**
+ * Created by michaelford on 9/13/16.
+ */
+public class Mean {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int length = in.nextInt();
+        int[] numbers = new int[length];
+        for(int index = 0; index < length; index++){
+            numbers[index] = in.nextInt();
+        }
+        int sum = 0;
+        for(int i = 0; i < length; i++){
+            sum += numbers[i];
+        }
+        double mean = (sum)/length;
+
+        double median = ((numbers[length/2-1] + numbers[length/2-2]))/length;
+        System.out.println(mean);
+        System.out.println(median);
+    }
+}
