d5d968c4fa9691db459be2c8b822b7ee6bf017f1
fordm13@uw.edu <husky6tcss360@gmail.com>
2016-09-15 15:33:21 -0700

    Used float to make Standard Deviation work

--- src/day1/StandardDeviation.java
+++ src/day1/StandardDeviation.java
@@ -1,7 +1,42 @@
 package day1;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+
 /**
  * Created by michaelford on 9/14/16.
  */
 public class StandardDeviation {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+        int[] numbers = new int[N];
+        int sum = 0;
+        for(int i = 0; i < N; i++){
+            numbers[i] = in.nextInt();
+            sum+= numbers[i];
+        }
+
+        standDev(sum, N, numbers);
+
+    }
+
+    static void standDev(int sum, int N, int[] numbers){
+        DecimalFormat df = new DecimalFormat("#.#");
+        double mean = (double) (sum/N);
+
+        float squaredDistance = 0;
+        for(int i = 0; i< N; i++){
+            squaredDistance += Math.pow((numbers[i] - mean),2);
+        }
+
+        double squareD = squaredDistance;
+
+        double sd = Double.valueOf(df.format(Math.sqrt(squareD/N)));
+
+        df.format(sd);
+        System.out.println(sd);
+    }
 }
